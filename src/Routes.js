c14912670c68fd4d3a054c59dc8847116ae1a824
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-01-13 21:37:01 +0300

    LogIn SignUp

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
 import Home from "./containers/Home";
+import LogIn from "./containers/LogIn";
+import SignUp from "./containers/SignUp";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={LogIn} />
+            <Route path="/signup" exact component={SignUp} />
             <Route component={NotFound} />
         </Switch>
     );
