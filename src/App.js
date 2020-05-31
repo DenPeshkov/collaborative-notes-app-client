1e6936d7d184caa5aad256c9fea41a11cb4d83fb
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-05-31 21:17:29 +0300

    added drawer and signin signup mobile optimised

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Routes from "./Routes";
 import {Layout} from "antd";
 import AppHeader from "./components/AppHeader/AppHeader";
 import {AppContext} from "./libs/contextLib";
+import {useMediaQuery} from 'react-responsive'
 
 const {Content} = Layout;
 
 function App() {
     const [isAuthenticated, userHasAuthenticated] = useState(false);
+    const isMobile = useMediaQuery({
+        query: '(max-width: 440px)'
+    })
 
     return (
         <Layout className="app-container">
-            <AppContext.Provider value={{isAuthenticated, userHasAuthenticated}}>
+            <AppContext.Provider value={{isAuthenticated, userHasAuthenticated, isMobile}}>
                 <AppHeader/>
                 <Layout>
                     <Content className="app-container-content">
--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import {Button, PageHeader} from "antd";
+import React, {useState} from 'react';
+import {Button, Drawer, Menu, PageHeader} from "antd";
 import {Link} from "react-router-dom";
 import {useAppContext} from "../../libs/contextLib";
 import "./AppHeader.css"
+import BarsOutlined from "@ant-design/icons/lib/icons/BarsOutlined";
+import LoginOutlined from "@ant-design/icons/lib/icons/LoginOutlined";
+import UserAddOutlined from "@ant-design/icons/lib/icons/UserAddOutlined";
 
 function AppHeader() {
 
-    const {isAuthenticated, userHasAuthenticated} = useAppContext();
+    const {isAuthenticated, userHasAuthenticated, isMobile} = useAppContext();
+    const [isDrawerVisible, setDrawerVisible] = useState(false);
+    const [menuState, setMenuState] = useState("signin");
     let extra;
 
     switch (isAuthenticated) {
         case false: {
-            extra = [
-                <Button key="1"><Link to="/signin">Sign in</Link></Button>,
-                <Button key="2" type="primary"><Link to="/signup">Sign up</Link></Button>,
-            ];
+            extra = isMobile ?
+                [
+                    <Button key="5" onClick={() => setDrawerVisible(true)}
+                            icon={<BarsOutlined style={{fontSize: "20px"}}/>}/>,
+                    <Drawer
+                        placement="right"
+                        onClose={() => setDrawerVisible(false)}
+                        visible={isDrawerVisible}
+                        closable={false}
+                    >
+                        <Menu onClick={(selectedKey) => {
+                            setMenuState(selectedKey.key);
+                            setDrawerVisible(false)
+                        }} selectedKeys={menuState}
+                              mode="inline">
+                            <Menu.Item key="signin" icon={<LoginOutlined/>}>
+                                <Link to="/signin">Sign in</Link>
+                            </Menu.Item>
+                            <Menu.Item key="signup" icon={<UserAddOutlined/>}>
+                                <Link to="/signup">Sign up</Link>
+                            </Menu.Item>
+                        </Menu>
+                    </Drawer>
+                ] :
+                [
+                    <Button key="3"><Link to="/signin">Sign in</Link></Button>,
+                    <Button key="4" type="primary"><Link to="/signup">Sign up</Link></Button>,
+                ];
             break;
         }
         case true: {
-            extra = [<Button ghost={true} key="1" type="primary" onClick={() => userHasAuthenticated(false)}>Log
+            extra = [<Button ghost={true} key="6" type="primary" onClick={() => userHasAuthenticated(false)}>Log
                 out</Button>];
             break;
         }
