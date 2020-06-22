52fd2034df9cd8dc1733559ea10950f9b98b6ee6
Peshkov Denis <peshkovdenis1985@gmail.com>
2020-06-22 22:51:53 +0300

    added Slate migrated to yarn

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,14 +1,23 @@
 import React, {useMemo, useState} from 'react';
 import "./Editor.css"
-import {createEditor} from "slate"
-import {Slate, withReact} from "slate-react"
+import {createEditor} from 'slate'
+import {Editable, Slate, withReact} from 'slate-react'
 
 function Editor() {
-    const editor = useMemo(() => withReact(createEditor()), []);
-    const [value, setValue] = useState([]);
+    const editor = useMemo(() => withReact(createEditor()), [])
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{text: 'A line of text in a paragraph.'}],
+        }
+    ])
 
     return (
-        <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}/>
+        <Slate editor={editor} value={value} onChange={newValue => {
+            setValue(newValue);
+        }}>
+            <Editable/>
+        </Slate>
     );
 }
 
