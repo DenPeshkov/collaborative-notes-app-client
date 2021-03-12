a953e1a88725556eac28ea5cab685e82909ba01b
Peshkov <peshkovdenis1985@gmail.com>
2021-03-12 19:36:10 +0300

    added home button, signup and login buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import {PageHeader} from "antd";
+import {Button, PageHeader} from "antd";
 import Routes from "./Routes";
+import {Link} from "react-router-dom";
 
 function App() {
   return (
       <div className="App">
         <PageHeader
             className="app-header"
-            title="ShareNotes"
+            title={
+              <Button className="title" key="1" type="link" href="/">
+                <Link to="/">
+                  ShareNotes
+                </Link>
+              </Button>
+            }
+            extra={[
+              <Button key="2" type="primary">
+                <Link to="/signup">
+                  Signup
+                </Link>
+              </Button>,
+              <Button key="3" type="primary">
+                <Link to="/login">
+                  Login
+                </Link>
+              </Button>
+            ]}
         />
         <Routes/>
       </div>
